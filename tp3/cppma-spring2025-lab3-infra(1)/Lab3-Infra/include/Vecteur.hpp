fb0aeb87825175f366d35b3919824ca79a6116a2
robin <carpentierrobin25@gmail.com>
2026-03-06 15:27:07 +0100

    feat:tp3

---
+++ tp3/cppma-spring2025-lab3-infra(1)/Lab3-Infra/include/Vecteur.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <string> 
+
+class Vecteur {
+
+    // vecteur 3d de double
+    
+    private:
+        double x;
+        double y;
+        double z;
+
+    public:
+
+        Vecteur(double x, double y, double z);
+        Vecteur();
+
+        double getX();
+        double getY();
+        double getZ();
+
+        void setX(double x);
+        void setY(double y);
+        void setZ(double z);
+
+
+
+
+};
---
+++ tp3/cppma-spring2025-lab3-infra(1)/Lab3-Infra/src/Vecteur.cxx
@@ -0,0 +1,22 @@
+#include "../include/Vecteur.hpp"
+#include <string> 
+
+Vecteur::Vecteur(double x, double y, double z) {
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vecteur::Vecteur() {
+    this->x = 0;
+    this->y = 0;
+    this->z = 0;
+}
+
+double Vecteur::getX() {return x;}
+double Vecteur::getY() {return y;}
+double Vecteur::getZ() {return z;}
+
+void Vecteur::setX(double x) {this->x = x;}
+void Vecteur::setY(double y) {this->y = y;}
+void Vecteur::setZ(double z) {this->z = z;}
